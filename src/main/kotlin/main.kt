0dac4c9f9cf8b8084087f9e5ec3062ed60597501
Rodrigo Pimenta <rodrigospimenta@gmail.com>
2021-06-20 00:09:54 -0300

    Double, Int, calculations, var and val

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,12 @@
 fun main(args: Array<String>) {
     println("Welcome Papper Bank!")
-    val customer = "Isaac Newton"
-    println("")
+    val customer:String = "Isaac Newton"
+    val accountNumber:Int = 1234
+    var balance:Double = 0.0
+    balance = 100.0
+    balance += 200
+
+    println("customer $customer")
+    println("account number $accountNumber")
+    println("balance $balance")
 }
