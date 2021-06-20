f42ca8ad66948003cd9cb4d94e19332789103600
Rodrigo Pimenta <rodrigospimenta@gmail.com>
2021-06-20 09:53:40 -0300

    For,For DownTo,Break,Step and While

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,43 +1,105 @@
 fun main(args: Array<String>) {
     println("Welcome Papper Bank!")
-    val customer:String = "Isaac Newton"
-    val accountNumber:Int = 1234
-    var balance:Double = 0.0
 
-    println("customer $customer")
-    println("account number $accountNumber")
-    println("balance $balance")
+    println()
+    println("*** Normal range ***")
+    for (i in 1..3) {
 
+        val customer: String = "Isaac Newton $i"
+        val accountNumber: Int = 1000 + i
+        var balance: Double = i + 10.0
+
+        println("customer $customer")
+        println("account number $accountNumber")
+        println("balance $balance")
+        println()
+
+        //Lessons 1 to 3
+        //testConditions(balance)
+    }
+
+    println()
+    println("*** DownTo range ***")
+    for (i in 3 downTo 1) {
+
+        val customer: String = "Isaac Newton $i"
+        val accountNumber: Int = 1000 + i
+        var balance: Double = i + 10.0
+
+        println("customer $customer")
+        println("account number $accountNumber")
+        println("balance $balance")
+        println()
+    }
+
+    println()
+    println("*** DownTo range with step and break ***")
+    for (i in 5 downTo 1 step 2) {
+
+        if(i == 1){
+            break
+        }
+        val customer: String = "Isaac Newton $i"
+        val accountNumber: Int = 1000 + i
+        var balance: Double = i + 10.0
+
+        println("customer $customer")
+        println("account number $accountNumber")
+        println("balance $balance")
+        println()
+    }
+
+    println()
+    println("*** While ***")
+    var i = 1
+    while (i < 4){
+        val customer: String = "Isaac Newton $i"
+        val accountNumber: Int = 1000 + i
+        var balance: Double = i + 10.0
+
+        println("customer $customer")
+        println("account number $accountNumber")
+        println("balance $balance")
+        println()
+        i++
+    }
+}
+
+fun testConditions(balance: Double) {
+    var localbalance = balance
+
+    //Neutral
+    localbalance = 0.0
     // IF
-    if (balance > 0.0){
-        println("balance is positive")
-    } else if (balance == 0.0){
-        println("balance is neutral")
+    if (localbalance > 0.0) {
+        println("balance $localbalance is positive")
+    } else if (localbalance == 0.0) {
+        println("balance $localbalance is neutral")
     } else {
-        println("balance is negative")
+        println("balance $localbalance is negative")
     }
 
     // Positive
-    balance += 200
+    localbalance += 200
     // When
     when {
-        balance > 0.0 -> {
-            println("balance is positive")
+        localbalance > 0.0 -> {
+            println("balance $localbalance is positive")
         }
-        balance == 0.0 -> {
-            println("balance is neutral")
+        localbalance == 0.0 -> {
+            println("balance $localbalance is neutral")
         }
         else -> {
-            println("balance is negative")
+            println("balance $localbalance is negative")
         }
     }
 
     // Negative
-    balance = -200.0
+    localbalance = -200.0
     // When oneline
     when {
-        balance > 0.0 -> println("balance is positive")
-        balance == 0.0 -> println("balance is neutral")
-        else -> println("balance is negative")
+        localbalance > 0.0 -> println("balance $localbalance is positive")
+        localbalance == 0.0 -> println("balance $localbalance is neutral")
+        else -> println("balance $localbalance is negative")
     }
 }
