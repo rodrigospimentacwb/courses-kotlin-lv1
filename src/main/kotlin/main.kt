1c8278598fe2f49588aa55f884b87edfc8b6c0fc
Rodrigo Pimenta <rodrigospimenta@gmail.com>
2021-06-20 00:27:08 -0300

    IF, ELSE, WHEN

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,10 +3,41 @@ fun main(args: Array<String>) {
     val customer:String = "Isaac Newton"
     val accountNumber:Int = 1234
     var balance:Double = 0.0
-    balance = 100.0
-    balance += 200
 
     println("customer $customer")
     println("account number $accountNumber")
     println("balance $balance")
+
+    // IF
+    if (balance > 0.0){
+        println("balance is positive")
+    } else if (balance == 0.0){
+        println("balance is neutral")
+    } else {
+        println("balance is negative")
+    }
+
+    // Positive
+    balance += 200
+    // When
+    when {
+        balance > 0.0 -> {
+            println("balance is positive")
+        }
+        balance == 0.0 -> {
+            println("balance is neutral")
+        }
+        else -> {
+            println("balance is negative")
+        }
+    }
+
+    // Negative
+    balance = -200.0
+    // When oneline
+    when {
+        balance > 0.0 -> println("balance is positive")
+        balance == 0.0 -> println("balance is neutral")
+        else -> println("balance is negative")
+    }
 }
