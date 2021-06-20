b1fe0a91678c1ded880542e3267433c645d6a543
Rodrigo Pimenta <rodrigospimenta@gmail.com>
2021-06-20 10:21:01 -0300

    Class, attributes

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,121 +1,26 @@
 fun main(args: Array<String>) {
     println("Welcome Papper Bank!")
+    var accountNewton = Account()
+    accountNewton.customer = "Isaac Newton"
+    accountNewton.accountNumber = 1000
+    accountNewton.balance = 100.0
 
-    println()
-    println("*** Normal range ***")
-    for (i in 1..3) {
+    val accountCurrie = Account()
+    accountCurrie.customer = "Marie Curie"
+    accountCurrie.accountNumber = 1001
+    accountCurrie.balance = 350.20
 
-        val customer: String = "Isaac Newton $i"
-        val accountNumber: Int = 1000 + i
-        var balance: Double = i + 10.0
+    println(accountNewton.customer)
+    println(accountNewton.accountNumber)
+    println(accountNewton.balance)
 
-        println("customer $customer")
-        println("account number $accountNumber")
-        println("balance $balance")
-        println()
-
-        //Lessons 1 to 3
-        //testConditions(balance)
-    }
-
-    println()
-    println("*** DownTo range ***")
-    for (i in 3 downTo 1) {
-
-        val customer: String = "Isaac Newton $i"
-        val accountNumber: Int = 1000 + i
-        var balance: Double = i + 10.0
-
-        println("customer $customer")
-        println("account number $accountNumber")
-        println("balance $balance")
-        println()
-    }
-
-    println()
-    println("*** DownTo range with step and break ***")
-    for (i in 5 downTo 1 step 2) {
-
-        if(i == 1){
-            break
-        }
-        val customer: String = "Isaac Newton $i"
-        val accountNumber: Int = 1000 + i
-        var balance: Double = i + 10.0
-
-        println("customer $customer")
-        println("account number $accountNumber")
-        println("balance $balance")
-        println()
-    }
-
-    println()
-    println("*** While ***")
-    var i = 1
-    while (i < 4){
-        val customer: String = "Isaac Newton $i"
-        val accountNumber: Int = 1000 + i
-        var balance: Double = i + 10.0
-
-        println("customer $customer")
-        println("account number $accountNumber")
-        println("balance $balance")
-        println()
-        i++
-    }
-
-    println()
-    println("*** For until: ignore last interaction ***")
-    for (i in 1 until 5) {
-        println(i)
-    }
-
-    println()
-    println("*** Nested Loop ***")
-    loop@ for (i in 1..100) {
-        println("i $i")
-        for (j in 1..100) {
-            println("j $j")
-            if (j == 5) break@loop
-        }
-    }
+    println(accountCurrie.customer)
+    println(accountCurrie.accountNumber)
+    println(accountCurrie.balance)
 }
 
-fun testConditions(balance: Double) {
-    var localbalance = balance
-
-    //Neutral
-    localbalance = 0.0
-    // IF
-    if (localbalance > 0.0) {
-        println("balance $localbalance is positive")
-    } else if (localbalance == 0.0) {
-        println("balance $localbalance is neutral")
-    } else {
-        println("balance $localbalance is negative")
-    }
-
-    // Positive
-    localbalance += 200
-    // When
-    when {
-        localbalance > 0.0 -> {
-            println("balance $localbalance is positive")
-        }
-        localbalance == 0.0 -> {
-            println("balance $localbalance is neutral")
-        }
-        else -> {
-            println("balance $localbalance is negative")
-        }
-    }
-
-    // Negative
-    localbalance = -200.0
-    // When oneline
-    when {
-        localbalance > 0.0 -> println("balance $localbalance is positive")
-        localbalance == 0.0 -> println("balance $localbalance is neutral")
-        else -> println("balance $localbalance is negative")
-    }
+class Account {
+    var customer:String = ""
+    var accountNumber:Int = 0
+    var balance: Double = 0.0
 }
