865a52156e67185f42a20f569a068d17ebcc4e9f
Rodrigo Pimenta <rodrigospimenta@gmail.com>
2021-06-21 09:00:38 -0300

    Implementing deposit function

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,7 +8,7 @@ fun main(args: Array<String>) {
     val accountCurrie = Account()
     accountCurrie.customer = "Marie Curie"
     accountCurrie.accountNumber = 1001
-    accountCurrie.balance = 350.20
+    accountCurrie.balance = 350.0
 
     println(accountNewton.customer)
     println(accountNewton.accountNumber)
@@ -17,10 +17,23 @@ fun main(args: Array<String>) {
     println(accountCurrie.customer)
     println(accountCurrie.accountNumber)
     println(accountCurrie.balance)
+
+    println()
+    println("Depositing to Newton's account")
+    accountNewton.deposit(50.0)
+    println("New balance ${accountNewton.balance}")
+
+    println("Depositing to Currie's account")
+    accountCurrie.deposit(100.0)
+    println("New balance ${accountCurrie.balance}")
 }
 
 class Account {
     var customer:String = ""
     var accountNumber:Int = 0
     var balance: Double = 0.0
+
+    fun deposit(value:Double){
+        balance += value
+    }
 }
