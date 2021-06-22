c9ee1c3c51ff44b9b42fa808260859599aaf7804
Rodrigo Pimenta <rodrigospimenta@gmail.com>
2021-06-22 09:10:12 -0300

    Primary and Sencondary builder

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,9 @@
 fun main(args: Array<String>) {
     println("Welcome Papper Bank!")
-    var accountNewton = Account()
-    accountNewton.customer = "Isaac Newton"
-    accountNewton.accountNumber = 1000
+    var accountNewton = Account("Isaac Newton", 1000)
     accountNewton.deposit(100.0)
 
-    val accountCurrie = Account()
-    accountCurrie.customer = "Marie Curie"
-    accountCurrie.accountNumber = 1001
+    val accountCurrie = Account("Marie Curie", 1001)
     accountCurrie.deposit(350.0)
 
     println(accountNewton.customer)
@@ -26,9 +22,9 @@ fun main(args: Array<String>) {
     accountNewton.withdraw(100.0)
     println("New balance ${accountNewton.balance}")
     println("Transfer from Newton's account to Currie's account")
-    if(accountNewton.transfer(50.0,accountCurrie)){
+    if (accountNewton.transfer(50.0, accountCurrie)) {
         println("Transfer made")
-    }else{
+    } else {
         println("Fail transfer")
     }
     println("New balance ${accountNewton.balance}")
@@ -41,27 +37,41 @@ fun main(args: Array<String>) {
     println("New balance ${accountCurrie.balance}")
 }
 
-class Account {
-    var customer:String = ""
-    var accountNumber:Int = 0
+class Account(var customer: String,
+              var accountNumber: Int) {
+
+    // Constructor already declares variables using var
+//    var customer:String = customer
+//    var accountNumber:Int = accountNumber
+
     var balance: Double = 0.0
-        private set(value){
+        private set(value) {
             //word 'field' is a reserved name that indicates the internal value of an existing attribute in the class.
             field = value
         }
-    
-    fun deposit(value:Double){
+
+    // Secondary Builder, May contain too much logic
+//    constructor(customer:String, accountNumber: Int){
+//        this.customer = customer
+//        this.accountNumber = accountNumber
+//    }
+
+    init {
+        //Run some logic during the primary builder
+    }
+
+    fun deposit(value: Double) {
         balance += value
     }
 
-    fun withdraw(value: Double){
-        if(balance >= value){
+    fun withdraw(value: Double) {
+        if (balance >= value) {
             balance -= value
         }
     }
 
-    fun transfer(value:Double, destiny:Account): Boolean {
-        if(balance >= value){
+    fun transfer(value: Double, destiny: Account): Boolean {
+        if (balance >= value) {
             balance -= value
             destiny.deposit(value)
             return true
