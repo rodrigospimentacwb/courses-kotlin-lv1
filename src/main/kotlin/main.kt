f4c32acb91a0a02312da425896a60db2b3b5ec98
Rodrigo Pimenta <rodrigospimenta@gmail.com>
2021-06-22 09:28:20 -0300

    Labels and default value in construtor

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,10 @@
 fun main(args: Array<String>) {
     println("Welcome Papper Bank!")
-    var accountNewton = Account("Isaac Newton", 1000)
+    var accountNewton = Account("Isaac Newton")
     accountNewton.deposit(100.0)
 
-    val accountCurrie = Account("Marie Curie", 1001)
+    // 'accountNumber and customer' Labels to indicate builder fields
+    val accountCurrie = Account(accountNumber = 1001,customer = "Marie Curie")
     accountCurrie.deposit(350.0)
 
     println(accountNewton.customer)
@@ -37,28 +38,14 @@ fun main(args: Array<String>) {
     println("New balance ${accountCurrie.balance}")
 }
 
-class Account(var customer: String,
-              var accountNumber: Int) {
-
-    // Constructor already declares variables using var
-//    var customer:String = customer
-//    var accountNumber:Int = accountNumber
+class Account(val customer: String,
+              val accountNumber: Int = 0) {
+    // val accountNumber: Int = 0 add optional send accountNumber
 
     var balance: Double = 0.0
         private set(value) {
-            //word 'field' is a reserved name that indicates the internal value of an existing attribute in the class.
             field = value
         }
-
-    // Secondary Builder, May contain too much logic
-//    constructor(customer:String, accountNumber: Int){
-//        this.customer = customer
-//        this.accountNumber = accountNumber
-//    }
-
-    init {
-        //Run some logic during the primary builder
-    }
 
     fun deposit(value: Double) {
         balance += value
