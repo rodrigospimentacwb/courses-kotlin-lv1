2ccd1ab7299ebe6b0f9c528ea2ec137dfc737cc0
Rodrigo Pimenta <rodrigospimenta@gmail.com>
2021-06-22 09:01:08 -0300

    Field, Properties without getters and setters

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,49 +3,53 @@ fun main(args: Array<String>) {
     var accountNewton = Account()
     accountNewton.customer = "Isaac Newton"
     accountNewton.accountNumber = 1000
-    accountNewton.setBalance(100.0)
+    accountNewton.deposit(100.0)
 
     val accountCurrie = Account()
     accountCurrie.customer = "Marie Curie"
     accountCurrie.accountNumber = 1001
-    accountCurrie.setBalance(350.0)
+    accountCurrie.deposit(350.0)
 
     println(accountNewton.customer)
     println(accountNewton.accountNumber)
-    println(accountNewton.getBalance())
+    println(accountNewton.balance)
 
     println(accountCurrie.customer)
     println(accountCurrie.accountNumber)
-    println(accountCurrie.getBalance())
+    println(accountCurrie.balance)
 
     println()
     println("Depositing to Newton's account")
     accountNewton.deposit(50.0)
-    println("New balance ${accountNewton.getBalance()}")
+    println("New balance ${accountNewton.balance}")
     println("Withdraw money to Newton's account")
     accountNewton.withdraw(100.0)
-    println("New balance ${accountNewton.getBalance()}")
+    println("New balance ${accountNewton.balance}")
     println("Transfer from Newton's account to Currie's account")
     if(accountNewton.transfer(50.0,accountCurrie)){
         println("Transfer made")
     }else{
         println("Fail transfer")
     }
-    println("New balance ${accountNewton.getBalance()}")
+    println("New balance ${accountNewton.balance}")
 
     println("Depositing to Currie's account")
     accountCurrie.deposit(100.0)
-    println("New balance ${accountCurrie.getBalance()}")
+    println("New balance ${accountCurrie.balance}")
     println("Withdraw money to Currie's account")
     accountCurrie.withdraw(200.0)
-    println("New balance ${accountCurrie.getBalance()}")
+    println("New balance ${accountCurrie.balance}")
 }
 
 class Account {
     var customer:String = ""
     var accountNumber:Int = 0
-    private var balance: Double = 0.0
-
+    var balance: Double = 0.0
+        private set(value){
+            //word 'field' is a reserved name that indicates the internal value of an existing attribute in the class.
+            field = value
+        }
+    
     fun deposit(value:Double){
         balance += value
     }
@@ -63,15 +67,5 @@ class Account {
             return true
         }
         return false
-    }
-
-    fun getBalance(): Double {
-        return balance
-    }
-
-    fun setBalance(value: Double) {
-        if(balance >= 0){
-            balance = value
-        }
     }
 }
